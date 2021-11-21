fddcfedba5c73f9c226064619b55ce7bc3d56050
Anutthara <dilinia@codegen.net>
2021-11-22 02:11:08 +0530

    modify rasa form

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,59 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Text, List, Any, Dict
 
+from rasa_sdk import Tracker, FormValidationAction, Action
+from rasa_sdk.events import EventType
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
-# This is a simple example for a custom action which utters "Hello World!"
+ALLOWED_STYLES = ["digital", "traditional", "cartoon", "realistic"]
+ALLOWED_MEDIUMS = ["colored", "line", "water color", "color pencil", "pencil", "oil color"]
+ALLOWED_FIGURES = ["one", "two", "three", "four", "1","2","3","4","couple","single","family"]
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ValidateArtistForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_artist_form"
+
+    def validate_style(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `style` value."""
+
+        if slot_value.lower() not in ALLOWED_STYLES:
+            dispatcher.utter_message(text=f"We only provide styles: digital, traditional, cartoon, realistic")
+            return {"style": None}
+        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} drawing.")
+        return {"style": slot_value}
+
+    def validate_medium(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `medium` value."""
+
+        if slot_value not in ALLOWED_MEDIUMS:
+            dispatcher.utter_message(text=f"I don't recognize that type. We provide {'/'.join(ALLOWED_MEDIUMS)}.")
+            return {"medium": None}
+        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} drawing.")
+        return {"medium": slot_value}
+
+    def validate_figures(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `figures` value."""
+
+        if slot_value not in ALLOWED_FIGURES:
+            dispatcher.utter_message(text=f"Sorry. We don't provide more than 4 figures.")
+            return {"figures": None}
+        dispatcher.utter_message(text=f"OK! You want to have a {slot_value} figures/drawing.")
+        return {"figures": slot_value}
